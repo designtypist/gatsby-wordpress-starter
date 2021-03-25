8adf9fc8b41490fa568667d79c478d163b7c4283
James Chung <designtypist@gmail.com>
2021-03-25 16:23:00 -0400

    removed unnneccessary files and directories alongside fixing the base url for the WordPress website

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'DesignTypist',
+    title: 'Gatsby + WordPress Starter',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -9,13 +9,13 @@ module.exports = {
       resolve: 'gatsby-source-wordpress',
       options: {
         // The base url to your WP site.
-        baseUrl: `.env.${process.env.BASE_URL}`,
+        baseUrl: 'wp.designtypist.com',
         // WP.com sites set to true, WP.org set to false
         hostingWPCOM: false,
         // The protocol. This can be http or https.
         protocol: 'https',
         // Use 'Advanced Custom Fields' Wordpress plugin
-        useACF: true,
+        useACF: false,
         auth: {},
         // Set to true to debug endpoints on 'gatsby build'
         verboseOutput: false,
